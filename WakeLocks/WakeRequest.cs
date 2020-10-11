d16e3bc150851d4fbfbd800672e96f571c7a1c42
AG Stephan <godtheresnonamesleft@gmail.com>
2020-10-11 02:39:05 -0500

    Add ability to kill processes with locks

--- WakeLocks/WakeRequest.cs
+++ WakeLocks/WakeRequest.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Diagnostics;
+using System.IO;
+
 namespace WakeLocks
 {
     class WakeRequest
@@ -12,5 +15,39 @@ namespace WakeLocks
         public string FullName => $"{this.FriendlyName}: {this.RequesterType} {this.Executable}";
 
         public override string ToString() => this.FullName;
+
+        public bool Kill()
+        {
+            var exeName = Path.GetFileNameWithoutExtension(this.Executable);
+            var toKill = Process.GetProcessesByName(exeName);
+
+            Debugger.Log(0, null, $"Killing {toKill.Length} processes\n");
+
+            bool successfulKill = true;
+            foreach (var proc in toKill)
+            {
+                Debugger.Log(0, null, $"Closing process with pid {proc.Id} via closing main window");
+                proc.CloseMainWindow();
+            }
+
+            foreach (var proc in toKill)
+            {
+                var exited = proc.WaitForExit(2000);
+                if (!exited)
+                {
+                    Debugger.Log(0, null, $"Failed to close process with pid {proc.Id}; Killing...");
+                    proc.Kill();
+                    exited = proc.WaitForExit(1000);
+
+                    if (!exited)
+                    {
+                        Debugger.Log(0, null, $"Failed to kill process with pid {proc.Id}");
+                        successfulKill = false;
+                    }
+                }
+            }
+
+            return successfulKill;
+        }
     }
 }
